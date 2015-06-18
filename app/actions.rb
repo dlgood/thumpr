c15d777a19fba59397d510ff24684bb1107d49c7
dlgood <dlgood@goodpage.com>
2015-06-18 01:54:13 +0000

    Add new story submission form

--- app/actions.rb
+++ app/actions.rb
@@ -2,3 +2,25 @@
 get '/' do
   erb :index
 end
+
+get '/stories/new' do 
+  @story = Story.new
+  erb :'stories/new'
+end  
+
+post '/stories' do 
+  @story = Story.new(
+    role: params[:role],
+    goal: params[:goal],
+    benefit: params[:benefit],
+    fftext: params[:fftext],
+    assignee: params[:assignee],
+    priority: params[:priority]
+  )
+  if @story.save
+    redirect '/stories/all'
+    #should add callout to top of all stories saying story saved successfully
+  else
+    erb :'stories/new'
+  end
+end  

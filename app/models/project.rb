b223ded35e533346897156699ddea00adc3d9f4c
andrewhayter <andrewhayter@gmail.com>
2015-06-19 00:02:18 +0000

    Changed Team, Users, and Story models

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,6 @@
 class Project < ActiveRecord::Base
   has_many :stories
+  has_many :user
 
   validates :p_name, presence: true
 
--- app/models/story.rb
+++ app/models/story.rb
@@ -1,6 +1,7 @@
 class Story < ActiveRecord::Base
   belongs_to :user
   belongs_to :assignee, :class_name => "User"
+  belongs_to :team
 
   validates :role, :goal, :benefit, presence: true 
 

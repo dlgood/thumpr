164ebd1c0f4f5ee9bf34a79d31836d6bd1ecc68d
andrewhayter <andrewhayter@gmail.com>
2015-06-20 19:07:20 +0000

    user profile image upload and resize complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
   has_many :stories
   belongs_to :team
+  
+  mount_uploader :file, Uploader
 
   validates :name, :password, presence: true, length: {in:1..20}
   validates :email, uniqueness: true, length: {in: 4..20} 

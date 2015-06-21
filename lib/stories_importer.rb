6ccf84d436dc7c1cf9175900995d8edf2036db5f
andrewhayter <andrewhayter@gmail.com>
2015-06-21 00:21:46 +0000

    added more sample data to work with for prioritizing

--- lib/stories_importer.rb
+++ lib/stories_importer.rb
@@ -2,6 +2,12 @@ class StoriesImporter
   def import
     Story.create(user_id: 1, role: "Purchaser", goal: "Order extra cheese on half the pizza", benefit: "Cater to the people in my party who like a lot of cheese", fftext: "I have to convince these friends not to go to Papa Guido's", assignee_id: 2, priority: 1, team_id: 1)
     Story.create(user_id: 3, role: "Driver", goal: "Mark order as delivered", benefit: "Prevent other drivers from delivering another pizza", assignee_id: 3, priority: 2, team_id: 2)
-    Story.create(user_id: 2, role: "Manager", goal: "See monthly revenue  numbers", benefit: "Make better staffing decisions", assignee_id: 1, priority: 3, team_id: 3)
+    Story.create(user_id: 2, role: "Manager", goal: "See monthly revenue  numbers", benefit: "Make better staffing decisions", assignee_id: 1, priority: 4, team_id: 3)    
+    Story.create(user_id: 1, role: "Bootcamp Student", goal: "Order extra cheese on half the pizza", benefit: "Cater to the people in my party who like a lot of cheese", fftext: "I have to convince these friends not to go to Papa Guido's", assignee_id: 3, priority: 1, team_id: 1)
+    Story.create(user_id: 3, role: "Teacher", goal: "Mark order as delivered", benefit: "Prevent other drivers from delivering another pizza", assignee_id: 1, priority: 1, team_id: 2)
+    Story.create(user_id: 2, role: "Hobo", goal: "See monthly revenue  numbers", benefit: "Make better staffing decisions", assignee_id: 2, priority: 1, team_id: 3)
+    Story.create(user_id: 1, role: "Sith", goal: "Order extra cheese on half the pizza", benefit: "Cater to the people in my party who like a lot of cheese", fftext: "I have to convince these friends not to go to Papa Guido's", assignee_id: 2, priority: 1, team_id: 1)
+    Story.create(user_id: 3, role: "Whatever", goal: "Mark order as delivered", benefit: "Prevent other drivers from delivering another pizza", assignee_id: 3, priority: 2, team_id: 2)
+    Story.create(user_id: 2, role: "Person", goal: "Be a person", benefit: "Be a better person or something", assignee_id: 1, priority: 5, team_id: 3)
   end
 end
--- lib/users_importer.rb
+++ lib/users_importer.rb
@@ -3,5 +3,8 @@ class UsersImporter
     User.create(name: 'Andrew Hayter', email: 'ahayter@gmail.com', password: 'password', team_id: 1)
     User.create(name: 'Dave Good', email: 'dgood@gmail.com', password: 'password', team_id: 3)
     User.create(name: 'Brandon Huang', email: 'bhuang@gmail.com', password: 'password', team_id: 2)
+    User.create(name: 'Don Burks', email: 'dburks@lighthouselabs.ca', password: 'password', team_id: 1)
+    User.create(name: 'Khurram Virani', email: 'khurrum@lighthouselabs.ca', password: 'password', team_id: 2)
+    User.create(name: 'Monica O', email: 'mmonica@lighthouselabs.ca', password: 'password', team_id: 3)
   end
 end
